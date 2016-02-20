81a21bc99b3938f132f5e445c1e1d15d9596a3e5
Maria Siapera <mariasiapera@outlook.com>
2016-02-20 16:10:16 +0200

    Insert into db

--- server.js
+++ server.js
@@ -5,7 +5,28 @@ var mongo = require("mongodb").MongoClient,
 mongo.connect("mongodb://127.0.0.1/chat", function(err, db){
   if(err) throw err;
   client.on("connection", function(socket){
+
+    var col = db.collection("messages");
+
     console.log("Someone has connected.");
     //Wait for input
+    socket.on("input", function(data){
+      var name = data.name,
+          message = data.message,
+          whiteSpacePattern = /^\s*$/;
+
+          if(whiteSpacePattern.test(name) || whiteSpacePattern.test(message)){
+            console.log("Invalid");
+          } else {
+            //insert into db
+            col.insert({name:name, message:message}, function(){
+              console.log("Inserted...");
+            });
+
+          }
+
+
+
+    });
   });
 });

65122addab7eb14c797ac3da975b6277e61f5f5d
Maria Siapera <mariasiapera@outlook.com>
2016-02-20 15:55:28 +0200

    Connection and listening for connections

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 var mongo = require("mongodb").MongoClient,
     client = require("socket.io").listen(8080).sockets;
 //console.log(client);
+
+mongo.connect("mongodb://127.0.0.1/chat", function(err, db){
+  if(err) throw err;
+  client.on("connection", function(socket){
+    console.log("Someone has connected.");
+    //Wait for input
+  });
+});

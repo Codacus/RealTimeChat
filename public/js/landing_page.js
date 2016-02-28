dcb531e9d7cca1eb5ad4d3822caef041f1032a18
Konstantinos Douloudis <kdoul@MacBook-toutes-Konstantinos.local>
2016-02-28 23:08:30 +0200

    Initial commit after cleanup

---
+++ public/js/landing_page.js
@@ -0,0 +1,29 @@
+$(function(){	
+
+	var roomid="";
+
+	$(".createNewRoom").click(function () {
+    	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    	for( var i=0; i < 10; i++ )
+        	roomid += possible.charAt(Math.floor(Math.random() * possible.length));    	
+	
+		window.location.href = window.location  +roomid;
+	});
+
+	$(".RoomUrl").click(function() {
+      
+      	roomid = $(".copyroomurl").val();
+      
+      	roomid = roomid.slice(-10);
+      
+      	if(/[^a-zA-Z0-9]/.test( roomid )){
+      		roomid = "";
+      		alert("Please enter a valid link or 10 character long roomid.\n Eg.Fcer9vtsre");
+      	}
+      	
+      	else
+			window.location.href = window.location +roomid;
+      
+    });
+});
